d9932399e4404d6338d7354b3fff85f342b6b698
christian <christianlouche@yahoo.fr>
2016-02-28 19:03:30 +0100

    Map bug fix

--- www/js/controllers/ArticleDetailsController.js
+++ www/js/controllers/ArticleDetailsController.js
@@ -58,39 +58,38 @@ angular.module('Occazstreet.controllers')
     console.log("data"+$localStorage[Globals.USER_LOGGED]);
 
     ArticlesService.getArticleById(article).then(function (response) {
-
       //On sauvegarde l'article dans la memoire
       $localStorage['detailsArticle']=response.article;
 
       $ionicLoading.hide();
 
-      $scope.art = response.article;
-
+      $scope.art = response;
+      console.dir($scope.art)
       var imageF="";
       var articleTitre="";
       var articleDetails="";
 
 
       //  angular.forEach(article,function(art){
-      articleTitre= response.article.titre;
-      articleDetails= response.article.details;
-      $rootScope.articleDevise= response.article.devise.symbole;
-      $scope.articlePrix= response.article.prix;
-      if( response.article.images.length>0)
+      articleTitre= response.titre;
+      articleDetails= response.details;
+      $rootScope.articleDevise= response.devise.symbole;
+      $scope.articlePrix= response.prix;
+      if( response.images.length>0)
       {
-        imageF= response.article.images[0].cheminImage;
+        imageF= response.images[0].cheminImage;
       }
       $scope.map = {
-        center: { latitude: response.article.latitude, longitude: response.article.longitude },
+        center: { latitude: response.latitude, longitude: response.longitude },
         marker: {
           id:0,
-          coords:{latitude: response.article.latitude,longitude: response.article.longitude},
+          coords:{latitude: response.latitude,longitude: response.longitude},
           option:{draggable: false},
         },
         option:{draggable: false,panControl:false,scrollwheel:false,zoomControl:false},
         zoom: 16
       };
-      if(typeof $localStorage[Globals.USER_LOGGED]!=='undefined' &&  response.article.utilisateur.id==$localStorage[Globals.USER_LOGGED].id){
+      if(typeof $localStorage[Globals.USER_LOGGED]!=='undefined' &&  response.utilisateur.id==$localStorage[Globals.USER_LOGGED].id){
         $scope.currentUser=true
       }else
       {
