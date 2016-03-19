127f82b2748c1a37e353c21ab9f2c40a133953e4
pisix <leundeufranky@gmail.com>
2016-03-19 21:42:01 +0100

    ajout service de partage( twitter, google...)

---
+++ www/js/services/SharingService.js
@@ -0,0 +1,64 @@
+/**
+ * Created by fleundeu on 18/03/2016.
+ */
+angular.module('Occazstreet.services')
+  .service('SharingService', function($cordovaSocialSharing) {
+
+
+    this.shareTwitter=function(message,image,link)
+    {
+      $cordovaSocialSharing
+       .shareViaTwitter(message, image, link)
+       .then(function(result) {
+       // Success!
+       }, function(err) {
+        console.log("une erreur est survenue lors de l'authentification Twitter  " +err)
+       });
+
+    };
+
+    this.shareMail=function(messageMail,subjectMail)
+    {
+      $cordovaSocialSharing
+        .shareViaEmail(messageMail, subjectMail)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+    };
+
+    this.shareWhatsapp=function(message,image,link)
+    {
+      $cordovaSocialSharing
+        .shareViaWhatsApp(message, image, link)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+
+    };
+
+    this.shareFacebook=function(message,image,link)
+    {
+      $cordovaSocialSharing
+        .shareViaFacebook(message +" "+image, image, link)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+        });
+    };
+
+    this.shareSMS=function(message,number)
+    {
+      $cordovaSocialSharing
+        .shareViaSMS(message, number)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+
+    };
+  });
