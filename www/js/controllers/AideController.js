a8345a21a7c2dba0cbc6172ff570d03e42881367
pisix <leundeufranky@gmail.com>
2016-03-19 21:36:13 +0100

    ajout action

--- www/js/controllers/AideController.js
+++ www/js/controllers/AideController.js
@@ -5,6 +5,8 @@ angular.module('Occazstreet.controllers')
     .controller('AideController', function($scope,$stateParams, $timeout,Globals) {
 
     $scope.version=Globals.VERSION;
+    $scope.facebookPage=Globals.FACEBOOKPAGE;
+    $scope.twitterPage=Globals.TWITTERPAGE;
 
 }).controller('CguController', function($scope,$stateParams, $timeout) {
 
