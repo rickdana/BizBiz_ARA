9182d3900d80e8953cc27187a757d75dcd906bff
pisix <leundeufranky@gmail.com>
2016-03-20 23:50:46 +0100

    Affichage message si la liste des catéfories n'est pas accessible

--- www/js/controllers/CategoriesController.js
+++ www/js/controllers/CategoriesController.js
@@ -2,7 +2,7 @@
  * Created by fleundeu on 26/04/2015.
  */
 angular.module('Occazstreet.controllers')
-    .controller('CategoriesController', function($scope,$stateParams, $timeout,ArticlesService,Globals) {
+    .controller('CategoriesController', function($scope,$stateParams, $timeout,ArticlesService,Globals,$mdToast) {
 
         // Set Motion
         $timeout(function() {
@@ -24,6 +24,32 @@ angular.module('Occazstreet.controllers')
             $scope.categories=ArticlesService.getCategories();
 
         });
+
+        $scope.isDisabled = false;
+        $scope.buttonRaffraichirText="Raffraichir";
+        $scope.raffraichir=function()
+        {
+          $scope.isDisabled=true;
+          $scope.buttonRaffraichirText="Patientez...";
+          ArticlesService.getAllCategories().then(function () {
+            $scope.categories=ArticlesService.getCategories();
+
+          });
+
+          $timeout(function() {
+            $scope.isDisabled=false;
+            $scope.buttonRaffraichirText="Raffraichir";
+            if(!$scope.categories)
+            {
+              $mdToast.show(
+                $mdToast.simple()
+                  .textContent('Serveur inaccessible')
+                  .position("top" )
+                  .hideDelay(3000)
+              );
+            }
+          }, 5000);
+        };
 
 
 
